285dcad0b19e7d8d0b62d072ab524f16ec921e44
jcentTech <80941264+jcentTech@users.noreply.github.com>
2022-03-31 11:24:54 +0800

    Update Actual.py

--- Actual.py
+++ Actual.py
@@ -18,6 +18,7 @@ from tabulate import tabulate
 import platform
 import speedtest
 import wmi
+import socket
 
 
 
@@ -37,10 +38,10 @@ def reload():
     
     
     #Division of Canvas
-    canvas1 = Canvas(root, width=450, height=170, bg="black")
+    canvas1 = Canvas(root, width=500, height=170, bg="black")
     canvas1.grid(row=1,column=0)
     
-    canvas2 = Canvas(root, width=450, height=100, bg="black")
+    canvas2 = Canvas(root, width=500, height=100, bg="black")
     canvas2.grid(row=2,column=0)
     
     canvas3 = Canvas(root, width=450, height=170, bg="black")
@@ -49,19 +50,19 @@ def reload():
     canvas4 = Canvas(root, width=450, height=100, bg="black")
     canvas4.grid(row=2,column=1)
     
-    canvas5 = Canvas(root, width=450, height=70, bg="black")
+    canvas5 = Canvas(root, width=500, height=80, bg="black")
     canvas5.grid(row=3,column=0)
     
-    canvas6 = Canvas(root, width=450, height=70, bg="black")
+    canvas6 = Canvas(root, width=450, height=80, bg="black")
     canvas6.grid(row=3,column=1)
     
-    canvas7 = Canvas(root, width=450, height=150, bg="black")
+    canvas7 = Canvas(root, width=500, height=150, bg="black")
     canvas7.grid(row=4,column=0)
     
     canvas8 = Canvas(root, width=450, height=150, bg="black")
     canvas8.grid(row=4,column=1)
 
-    canvas9 = Canvas(root,width=900,height=100, bg="black")
+    canvas9 = Canvas(root,width=950,height=100, bg="black")
     canvas9.grid(row=5,column=0,columnspan=2)
 
     
@@ -86,6 +87,9 @@ def reload():
     computer = wmi.WMI()
     gpu_info = computer.Win32_VideoController()[0]
     proc_info = computer.Win32_Processor()[0]
+    os_info = computer.Win32_OperatingSystem()[0]
+    os_version = ' '.join([os_info.Version])
+    buildnumber_info = os_info.BuildNumber
     
 
 
@@ -99,7 +103,7 @@ def reload():
     canvas1.create_text(120,40,fill="green",font=("Ariel",12), text="Username: {}".format(monitor_username))
     canvas1.create_text(69,60,fill="green",font=("Ariel",12), text="System: {}".format(monitor_system_info.system))
     canvas1.create_text(48,80,fill="green",font=("Ariel",12), text="Release: {}".format(monitor_system_info.release))
-    canvas1.create_text(77,100,fill="green",font=("Ariel",12), text="Version: {}".format(monitor_system_info.version))
+    canvas1.create_text(150,100,fill="green",font=("Ariel",12), text=f"Version: {os_version}  Buildnumber: {buildnumber_info}")
     canvas1.create_text(67,120,fill="green",font=("Ariel",12), text="Machine: {}".format(monitor_system_info.machine))
     canvas1.create_text(230,140,fill="green",font=("Ariel",12), text="Processor: {}".format(monitor_system_info.processor))
     
@@ -107,7 +111,7 @@ def reload():
     canvas2.create_text(115,50,fill="green",font=("Ariel",12), text=(f"Date: {monitor_boot_date.year}/{monitor_boot_date.month}/{monitor_boot_date.day} Time:{monitor_boot_date.hour}:{monitor_boot_date.minute}:{monitor_boot_date.second}"))
     
     canvas3.create_text(40, 15,fill="green",font=("Ariel",12,font.BOLD), text="CPU Util ")
-    canvas3.create_text(65, 30,fill="green",font=("Ariel",12), text="Processor: {}".format(proc_info.Name))
+    canvas3.create_text(195, 30,fill="green",font=("Ariel",12), text="Processor: {}".format(proc_info.Name))
     canvas3.create_text(55, 50,fill="green",font=("Ariel",12), text="CPU Cores: {}".format(monitor_cpu_cores))
     canvas3.create_text(73, 70,fill="green",font=("Ariel",12), text="CPU Usage: {}".format(monitor_cpu_usage)+ "%")
     canvas3.create_text(126, 90,fill="green",font=("Ariel",12), text="CPU Max Frequency: {:.2f}Mhz".format(monitor_cpu_freq.max))
@@ -126,21 +130,24 @@ def reload():
     canvas6.create_text(145,60,fill="green",font=("Ariel",12), text="Total Bytes Received: {:0.2f} Kbps".format(monitor_network_recv/1024))
     
     canvas7.create_text(30,15, fill="green",font=("Ariel",12, font.BOLD), text="GPU")
-    canvas7.create_text(105,40, fill="green",font=("Ariel",12, font.BOLD), text=gpu_info.Name)
+    canvas7.create_text(105,40, fill="green",font=("Ariel",12), text=gpu_info.Name)
 
     canvas8.create_text(80,15, fill="green",font=("Ariel",12, font.BOLD), text="Internet Speed Test")
 
-    mylabel1 = tk.Label(fg="green",font=("Ariel,",12,font.BOLD),text = "Click the speed test to run!",bg="black")
-    mylabel1.place(x=460,y=380)
+    mylabel1 = tk.Label(fg="green",font=("Ariel,",12),text = "Click the speed test to run!",bg="black")
+    mylabel1.place(x=510,y=390)
 
     mylabel2 = tk.Label(text = "",bg="black")
-    mylabel2.place(x=460,y=400)
+    mylabel2.place(x=510,y=410)
 
     mylabel3 = tk.Label(text = "",bg="black")
-    mylabel3.place(x=460,y=420)
+    mylabel3.place(x=510,y=430)
 
     mylabel4 = tk.Label(text = "",bg="black")
-    mylabel4.place(x=460,y=440)
+    mylabel4.place(x=510,y=450)
+
+    mylabel5 = tk.Label(text = "",bg="black")
+    mylabel5.place(x=510,y=470)
 
    
 
@@ -157,13 +164,16 @@ def reload():
         download_result = test.download()
         upload_result = test.upload()
         ping_result = test.results.ping
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
 
         
         
-        mylabel1.config(fg="green",font=("Ariel",12),text = f"Found: {best['host']} Located in {best['country']}")
+        mylabel1.config(fg="green",font=("Ariel",12),text = f"Server: {best['host']} Located in {best['country']}")
         mylabel2.config(fg="green",font=("Ariel",12),text =f"Download Speed: {download_result / 1024 / 1024:.2f} Mbit/s")
         mylabel3.config(fg="green",font=("Ariel",12),text =f"Upload Speed: {upload_result / 1024 / 1024:.2f} Mbit/s")
         mylabel4.config(fg="green",font=("Ariel",12),text =f"Ping: {ping_result:.2f} ms")
+        mylabel5.config(fg="green",font=("Ariel",12),text =f"IP address: {ip_address}")
 
 
     btn_refresh = tk.Button(root, text="Speed Test", bg="Blue", fg="white",command=speed_test)
